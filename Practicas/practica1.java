552e8f41a090a5ad1a09414e5f1c920913465bd9
Nagi2880 <alexanderamaral17@gmail.com>
2023-02-01 07:29:47 -0300

    a

---
+++ Practicas/practica1.java
@@ -0,0 +1,12 @@
+
+public class practica1{
+    public static void main(String args[]){
+        int numero1= 2;
+        int numero2= 2;
+        int resultado= 0;
+
+        resultado = numero1 + numero2;
+        System.out.println("El valor de la suma es " + resultado);
+
+    }
+}
---
+++ hello world/mainsu.java
@@ -0,0 +1,5 @@
+public class mainsu{
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
